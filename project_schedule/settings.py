2c4718b57ddc094cfa139755fd4ecf03e450afb7
kshitijsingh <kshitijsingh661@gmail.com>
2020-07-08 23:21:34 +0530

    added static storage

--- project_schedule/settings.py
+++ project_schedule/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -121,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
